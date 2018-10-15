b9527d195963575cbb5c4b0e6a5b4671980b392c
JuniorFarias <junior13farias@gmail.com>
2018-10-15 14:28:49 -0300

    Primeiro commit de python3

---
+++ readme.txt
@@ -0,0 +1,1 @@
+Resoluções de problemas do site URI Online Judge na linguagem python3
---
+++ uri1008.py
@@ -0,0 +1,6 @@
+NUMBER = int(input())
+horasTrabalhadas = int(input())
+valorHora = float(input())
+SALARY = horasTrabalhadas * valorHora
+print('NUMBER =', NUMBER)
+print('SALARY = U$ %.2f' % SALARY)
